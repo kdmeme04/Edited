087536c903b7217570f5c6a753eda5e04f7b0e79
Logesh <116418351+LPSMOVIES@users.noreply.github.com>
2022-11-29 07:24:32 +0530

    Update commands.py

--- plugin/commands.py
+++ plugin/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN NOW', url='https://t.me/Cps_HD') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
